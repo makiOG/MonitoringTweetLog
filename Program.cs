adc44c04bfb8225a91270c46ef5830d8da232a14
maki <>
2020-01-03 23:47:43 +0900

    sync void関数を追加する

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.IO;
+using System.Threading;
 
 namespace twittertest
 {
@@ -25,50 +26,10 @@ namespace twittertest
             var parm = new Dictionary<string, object>();  //条件指定用Dictionary
             parm["count"] = 5;  //取得数
             parm["screen_name"] = "makiog33";  //取得したいユーザーID
-            //makiog33
-            //makiog3
+                                               //makiog33
+                                               //makiog3
 
-            Task task = Task.Factory.StartNew(async () =>
-            {
-                var tweets = await tokens.Statuses.UserTimelineAsync(parm);
-
-                foreach (var tweet in tweets)
-                {
-                    if (tweet.Entities.Media != null && tweet.Entities.Media.Length != 0)
-                    {
-                        var img_count = tweet.ExtendedEntities.Media.Length;
-                        for (; img_count > 0; img_count--)
-                        {
-                            var url = tweet.ExtendedEntities.Media[img_count - 1].MediaUrlHttps;
-                            //for文だと先に引かれないためimg_countを-1している
-                            //while文だと先に-1する処理にできるが、一文増えるので多分for文の記述のほうがスッキリする
-                            var img_name = tweet.ExtendedEntities.Media[img_count - 1].Id;
-                            
-                            Console.WriteLine(url + "\n");
-                            //フォルダ指定 適当に絶対パス指定にしてるけど相対パス指定にする
-                            DownloadImgAsync(url,
-                                @"C:\新しいフォルダー\" + img_name + ".png");
-
-                        }
-
-                        Console.WriteLine(tweet.User.Name + "(@" + tweet.User.ScreenName + ")\r\n" + tweet.Text + "\r\n(at " + tweet.CreatedAt + ")" + " media_count " + tweet.ExtendedEntities.Media.Length + "\r\n");
-                        Console.WriteLine("--------------");
-
-                    }
-                    //メディアなしの場合
-                    //elseで同じような処理を括るのは気持ち悪いので後で直したい
-                    else
-                    {
-                        Console.WriteLine(tweet.User.Name + "(@" + tweet.User.ScreenName + ")\r\n" + tweet.Text + "\r\n(at " + tweet.CreatedAt + ")" + " media_count 0" + "\r\n");
-                        Console.WriteLine("--------------");
-                    }
-
-                    
-
-                }
-            }).Unwrap();
-
-            task.Wait();
+            Test(tokens,parm);
 
             Console.ReadLine();
         }
@@ -85,6 +46,42 @@ namespace twittertest
             using (var fileStream = File.Create(outputPath))
             using (var httpStream = await res.Content.ReadAsStreamAsync())
                 httpStream.CopyTo(fileStream);
+        }
+        static async void Test(CoreTweet.Tokens tokens, Dictionary<string, object> parm)
+        {
+            var tweets = await tokens.Statuses.UserTimelineAsync(parm);
+
+            foreach (var tweet in tweets)
+            {
+                if (tweet.Entities.Media != null && tweet.Entities.Media.Length != 0)
+                {
+                    var img_count = tweet.ExtendedEntities.Media.Length;
+                    for (; img_count > 0; img_count--)
+                    {
+                        var url = tweet.ExtendedEntities.Media[img_count - 1].MediaUrlHttps;
+                        //for文だと先に引かれないためimg_countを-1している
+                        //while文だと先に-1する処理にできるが、一文増えるので多分for文の記述のほうがスッキリする
+                        var img_name = tweet.ExtendedEntities.Media[img_count - 1].Id;
+
+                        Console.WriteLine(url + "\n");
+                        //フォルダ指定 適当に絶対パス指定にしてるけど相対パス指定にする
+                        DownloadImgAsync(url,
+                            @"C:\新しいフォルダー\" + img_name + ".png");
+
+                    }
+
+                    Console.WriteLine(tweet.User.Name + "(@" + tweet.User.ScreenName + ")\r\n" + tweet.Text + "\r\n(at " + tweet.CreatedAt + ")" + " media_count " + tweet.ExtendedEntities.Media.Length + "\r\n");
+                    Console.WriteLine("--------------");
+
+                }
+                //メディアなしの場合
+                //elseで同じような処理を括るのは気持ち悪いので後で直したい
+                else
+                {
+                    Console.WriteLine(tweet.User.Name + "(@" + tweet.User.ScreenName + ")\r\n" + tweet.Text + "\r\n(at " + tweet.CreatedAt + ")" + " media_count 0" + "\r\n");
+                    Console.WriteLine("--------------");
+                }
+            }
         }
     }
 }
